8809941bd77217861402a81ee8b084772a250111
jbuzzanco <jcbuzzanco@gmail.com>
2017-01-14 11:38:36 -0500

    adds application adapter host to localhost
    adds application adapter host to http://localhost:7165
    running locally still works for auth sign-in

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,22 +1,5 @@
-import ENV from 'ga-wdi-boston.ember-auth/config/environment';
-import ActiveModelAdapter from 'active-model-adapter';
+import DS from 'ember-data';
 
-import Ember from 'ember';
-
-export default ActiveModelAdapter.extend({
-  host: ENV.apiHost,
-
-  auth: Ember.inject.service(),
-
-  headers: Ember.computed('auth.credentials.token', {
-    get () {
-      let headers = {};
-      const token = this.get('auth.credentials.token');
-      if (token) {
-        headers.Authorization = `Token token=${token}`;
-      }
-
-      return headers;
-    },
-  }),
+export default DS.JSONAPIAdapter.extend({
+  host: 'http://localhost:7165'
 });

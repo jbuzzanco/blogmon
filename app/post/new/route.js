3cbb94f04d5a0d919415a03bdfd9ef4bdacd5a29
jbuzzanco <jcbuzzanco@gmail.com>
2017-01-17 13:15:36 -0500

    adds create post.
    create post is now working from the front
    end once you are signed into the user account.
    Does not transition to the post index, but to the post it created.

--- app/post/new/route.js
+++ app/post/new/route.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {};
+  },
+  actions: {
+    save() {
+        const newPost = this.get('store').createRecord('post', this.currentModel);
+        newPost.save().then((post) => {
+          this.transitionTo('post', post);
+        });
+      },
+    cancel() {
+      this.transitionTo('posts');
+    },
+  },
 });

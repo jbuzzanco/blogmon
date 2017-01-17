f90eb9a9c013f5cf111f65f204c8f11ba7c67491
jbuzzanco <jcbuzzanco@gmail.com>
2017-01-17 16:50:06 -0500

    adds files to change app-name
    to blogmon-ember

--- app/posts/route.js
+++ app/posts/route.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    console.log("Is this running?");
     return this.get('store').findAll('post');
   },
   actions: {

d9e593386161108c52b89ec746165f3c4cd68601
jbuzzanco <jcbuzzanco@gmail.com>
2017-01-19 15:55:55 -0500

    adds if post editable to delete.
    so that only one user can delete whatever they
    created

--- app/post/model.js
+++ app/post/model.js
@@ -6,6 +6,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   body: DS.attr('string'),
-  user: DS.belongsTo('user'),
+  // user: DS.belongsTo('user'),
+  editable: DS.attr('boolean')
   // comments: DS.hasMany('comment')
 });
